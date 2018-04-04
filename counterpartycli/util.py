52209acfe2538082aea82d86677d19516a66460a
Yoshihiro Takahara <30272500+tumfwkm@users.noreply.github.com>
2018-04-04 14:59:17 +0700

    Remove clean BOM codes
    Avoid to open as writable

--- counterpartycli/util.py
+++ counterpartycli/util.py
@@ -172,23 +172,6 @@ def add_config_arguments(arg_parser, config_args, default_config_file, config_fi
             os.makedirs(config_dir, mode=0o755)
         config_file = os.path.join(config_dir, default_config_file)
 
-    # clean BOM
-    BUFSIZE = 4096
-    BOMLEN = len(codecs.BOM_UTF8)
-    with codecs.open(config_file, 'r+b') as fp:
-        chunk = fp.read(BUFSIZE)
-        if chunk.startswith(codecs.BOM_UTF8):
-            i = 0
-            chunk = chunk[BOMLEN:]
-            while chunk:
-                fp.seek(i)
-                fp.write(chunk)
-                i += len(chunk)
-                fp.seek(BOMLEN, os.SEEK_CUR)
-                chunk = fp.read(BUFSIZE)
-            fp.seek(-BOMLEN, os.SEEK_CUR)
-            fp.truncate()
-
     logger.debug('Loading configuration file: `{}`'.format(config_file))
     configfile = configparser.SafeConfigParser(allow_no_value=True, inline_comment_prefixes=('#', ';'))
     with codecs.open(config_file, 'r', encoding='utf8') as fp:
